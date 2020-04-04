8cb2c998d7e936b825e86c1e5e9b6c2b18926a45
Łukasz Pelczar <luk085@gmail.com>
2020-04-04 14:00:54 +0200

    add audio support

--- stream.sh
+++ stream.sh
@@ -2,7 +2,7 @@
 RTSP_URL=""
 YOUTUBE_URL="rtmp://a.rtmp.youtube.com/live2"
 YOUTUBE_KEY=""
-COMMAND="ffmpeg -f lavfi -i anullsrc -rtsp_transport tcp -i ${RTSP_URL} -tune zerolatency -vcodec libx264 -t 12:00:00 -pix_fmt + -c:v copy -c:a aac -strict experimental -f flv ${YOUTUBE_URL}/${YOUTUBE_KEY}"
+COMMAND="ffmpeg -rtsp_transport tcp -i ${RTSP_URL} -tune zerolatency -vcodec libx264 -t 12:00:00 -pix_fmt + -c:v copy -c:a aac -f flv ${YOUTUBE_URL}/${YOUTUBE_KEY}"
 
 case $1 in
   start)
